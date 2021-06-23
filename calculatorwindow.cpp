fca983fc14d3e06137bbefdd44c97588607210f3
Wiktor W <systemax123@outlook.com>
2021-06-23 15:52:49 +0100

    All buttons on calculator now display their respective symbol

--- calculatorwindow.cpp
+++ calculatorwindow.cpp
@@ -17,10 +17,118 @@ CalculatorWindow::~CalculatorWindow()
 
 void CalculatorWindow::on_Zero_clicked()
 {
+    expression += "0";
+    ui->numberDisplay->setText(expression);
 }
 
 
 void CalculatorWindow::on_Evaluate_clicked()
 {
+    ui->numberDisplay->setText(QString::fromStdString("1 + 2"));
+}
+
+
+void CalculatorWindow::on_One_clicked()
+{
+    expression += "1";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Two_clicked()
+{
+    expression += "2";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Three_clicked()
+{
+    expression += "3";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Four_clicked()
+{
+    expression += "4";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Five_clicked()
+{
+    expression += "5";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Six_clicked()
+{
+    expression += "6";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Seven_clicked()
+{
+    expression += "7";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Eight_clicked()
+{
+    expression += "8";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Nine_clicked()
+{
+    expression += "9";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Add_clicked()
+{
+    expression += "+";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Subtract_clicked()
+{
+    expression += "-";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Multiply_clicked()
+{
+    expression += "*";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_Divide_clicked()
+{
+    expression += "/";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_LeftBracket_clicked()
+{
+    expression += "(";
+    ui->numberDisplay->setText(expression);
+}
+
+
+void CalculatorWindow::on_RightBracket_clicked()
+{
+    expression += ")";
+    ui->numberDisplay->setText(expression);
 }
 
--- calculatorwindow.h
+++ calculatorwindow.h
@@ -20,7 +20,38 @@ private slots:
 
     void on_Evaluate_clicked();
 
+    void on_One_clicked();
+
+    void on_Two_clicked();
+
+    void on_Three_clicked();
+
+    void on_Four_clicked();
+
+    void on_Five_clicked();
+
+    void on_Six_clicked();
+
+    void on_Seven_clicked();
+
+    void on_Eight_clicked();
+
+    void on_Nine_clicked();
+
+    void on_Add_clicked();
+
+    void on_Subtract_clicked();
+
+    void on_Multiply_clicked();
+
+    void on_Divide_clicked();
+
+    void on_LeftBracket_clicked();
+
+    void on_RightBracket_clicked();
+
 private:
     Ui::CalculatorWindow *ui;
+    QString expression;
 };
 #endif // CALCULATORWINDOW_H
