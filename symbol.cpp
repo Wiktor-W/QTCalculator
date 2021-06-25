b2d271a4b1a0f874526c449a717ab5b70206dbcc
Wiktor W <systemax123@outlook.com>
2021-06-25 14:10:43 +0100

    Added isNumber and isCharacter fields to symbol class to make differentiating easier

--- symbol.cpp
+++ symbol.cpp
@@ -6,26 +6,38 @@ Symbol::Symbol(char newSymbol)
       case '(':
         symbol = newSymbol;
         priority = 1;
+        isNum = false;
+        isChar = true;
         break;
       case ')':
         symbol = newSymbol;
         priority = 1;
+        isNum = false;
+        isChar = true;
         break;
       case '/':
         symbol = newSymbol;
         priority = 2;
+        isNum = false;
+        isChar = true;
         break;
       case '*':
         symbol = newSymbol;
         priority = 3;
+        isNum = false;
+        isChar = true;
         break;
       case '+':
         symbol = newSymbol;
         priority = 4;
+        isNum = false;
+        isChar = true;
         break;
       case '-':
         symbol = newSymbol;
         priority = 5;
+        isNum = false;
+        isChar = true;
         break;
       default:
         symbol = newSymbol;
@@ -35,6 +47,8 @@ Symbol::Symbol(char newSymbol)
 Symbol::Symbol(int newValue)
 {
     value = newValue;
+    isNum = true;
+    isChar = false;
 }
 Symbol::Symbol(){
     value = 0;
--- symbol.h
+++ symbol.h
@@ -8,13 +8,17 @@ private:
     int value;
     char symbol;
     int priority;
+    bool isNum;
+    bool isChar;
 public:
     Symbol(char newChar);
     Symbol(int newValue);
     Symbol();
-    char getChar(){ return symbol; };
-    int getValue(){ return value; };
-    int getPriority(){ return priority; };
+    char getChar() const { return symbol; };
+    int getValue() const { return value; };
+    int getPriority() const { return priority; };
+    bool isNumber() const { return isNum; };
+    bool isCharacter() const { return isChar; };
 };
 
 #endif // SYMBOL_H
